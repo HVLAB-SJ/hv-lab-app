0bd240b8fcd8faf9535176b701bba0ce372a5723
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-30 08:49:22 +0900

    결제요청: 세로모드 폼 레이아웃 개선 - 프로젝트/텍스트/버튼 세로배치

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-DwWODczK.js",
+    "url": "assets/index-InZ-yCqw.css",
     "revision": null
   }, {
-    "url": "assets/index-N1-ZkWNU.css",
+    "url": "assets/index-tnENdyzQ.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "b853a33468901964eca8bd4da3743838"
+    "revision": "fdd43182f7dafcf60870578685d7172d"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
