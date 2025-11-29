125db9ed33e910ef791d6aa300556053ee83cdfa
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-30 08:41:55 +0900

    결제요청: 세로모드 레이아웃 - 폼 상단, 목록 중단, 이미지 하단

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-B2Z4oEQZ.js",
+    "url": "assets/index-DwWODczK.js",
     "revision": null
   }, {
-    "url": "assets/index-D5h20NDT.css",
+    "url": "assets/index-N1-ZkWNU.css",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "1c49e540b625b59ace9c308e2b9fa334"
+    "revision": "b853a33468901964eca8bd4da3743838"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
