ccd643ef3f88dc7b060eeee8661ff147e87f6632
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-30 02:48:27 +0900

    실행내역: 세로모드 입력카드 높이 증가

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-DFTcITnb.css",
+    "url": "assets/index-CGpXBPBr.css",
     "revision": null
   }, {
-    "url": "assets/index-oSubXtgz.js",
+    "url": "assets/index-DjqSnazQ.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "377e18bc9dffb7e69f929d6cd9a34dad"
+    "revision": "a257ef648fa04cf957d1596e6cf846cc"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
