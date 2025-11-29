ca45a7d83f874a577f360ae79fbdc775646f284a
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-29 23:39:09 +0900

    실행내역 데스크탑 가운데 카드 너비 확대 (2:6:4)

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-BeDT_NIe.css",
+    "url": "assets/index-B5-0wtR1.js",
     "revision": null
   }, {
-    "url": "assets/index-CS0HB3vc.js",
+    "url": "assets/index-BeDT_NIe.css",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "6e41240ff62ceadd86357647e0f12d58"
+    "revision": "6fbe4b62d485841654814bfe83bad926"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
