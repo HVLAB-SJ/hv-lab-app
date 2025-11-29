4de1c370632b58b0e48794083526f226cc504755
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-30 01:44:18 +0900

    스펙북: 세로모드 카테고리 5열 및 너비 확대

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-BjscNR7M.js",
+    "url": "assets/index-B5WMXptG.js",
     "revision": null
   }, {
-    "url": "assets/index-CXCn6l6f.css",
+    "url": "assets/index-D7Y6793I.css",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "9fb0fec2c003f98eb951862541cfb897"
+    "revision": "a3a77265a48ea99728c6a9acb7001ab9"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
