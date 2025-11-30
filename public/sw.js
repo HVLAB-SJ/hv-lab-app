80bf9401104657876baf113e78e84774f41429c9
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-12-01 01:13:54 +0900

    스펙북: 세로모드 등급필터를 이미지삽입 아래로 이동

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-BNCv5Bwd.css",
+    "url": "assets/index-c1yclpM3.js",
     "revision": null
   }, {
-    "url": "assets/index-DCxXCFOj.js",
+    "url": "assets/index-CKZTzvcZ.css",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "6ebfe0481f52d6921983719252363077"
+    "revision": "a85dc40f9c8aab16fd444fdfae168925"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
