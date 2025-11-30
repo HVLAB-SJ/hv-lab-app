e6ce1948557607cfb51189d489ef9da93dff77e4
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-30 10:27:48 +0900

    결제요청: 세로모드 예금주+결제버튼 같은 행 배치, 여백 최적화

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-BCNhEIpI.js",
+    "url": "assets/index-D1tJhnFH.css",
     "revision": null
   }, {
-    "url": "assets/index-CNBPWcco.css",
+    "url": "assets/index-xlHmy-hh.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "230e2a1d172dafebe31f7a6c590fece7"
+    "revision": "1b9291e1574c32af6a21a30bd1599c64"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
