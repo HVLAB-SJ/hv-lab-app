8e0e77033d27f2b23902f0405d7b39121bc60038
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-30 09:05:07 +0900

    결제요청: 카드 영역 내 고정 높이로 overflow 방지

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-B4vZF3OD.css",
+    "url": "assets/index-D17xFMYL.css",
     "revision": null
   }, {
-    "url": "assets/index-D4GdEQeN.js",
+    "url": "assets/index-DK9lmYnJ.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "aead5d601abfda63a6564e6dcc4c0dd7"
+    "revision": "8c892a20336dd36034d28d594fc81938"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
