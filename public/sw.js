bc0234ea3645601ab7587653ddaf9f030951f8f0
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-12-01 00:46:45 +0900

    업무요청: 세로모드 요청내용/버튼 좌우 배치

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-CPR0IH2s.css",
+    "url": "assets/index-6nadWl0p.css",
     "revision": null
   }, {
-    "url": "assets/index-DAU8tcXJ.js",
+    "url": "assets/index-DGLDFFJt.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "b78d8eea0e004d69f74c48499903ea02"
+    "revision": "9d9590eec3d7ebaaa87ac859496a1e9f"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
