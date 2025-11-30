9430a81c29c7ad11fcfe48b9c5b082ca395d302c
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-30 18:19:13 +0900

    결제요청: 세로모드 왼쪽열 40%, 오른쪽열 60%로 조정

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-CW_LeCuB.css",
+    "url": "assets/index-BfgIgRXW.css",
     "revision": null
   }, {
-    "url": "assets/index-DuV1Wv33.js",
+    "url": "assets/index-Ci6E4t4Y.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "e0e71437f2303d68c5e31f2c2ba1b950"
+    "revision": "87b49d47ea59a330f6dce77ec7b92214"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
