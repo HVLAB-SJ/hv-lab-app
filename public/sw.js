3cfda82f76ed4e92ddf4b55c0a42ba6f66e9b958
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-12-01 00:53:31 +0900

    일정표: 세로모드 높이 -180px로 조정

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-CJkA9-LG.js",
+    "url": "assets/index-D2Fyl1XJ.js",
     "revision": null
   }, {
-    "url": "assets/index-CUpKjp1o.css",
+    "url": "assets/index-DQG_594W.css",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "bd8f41961e2dfa616e1b36ead225e299"
+    "revision": "77c789708f1dbd2c5de0be7fbd94c4a6"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
