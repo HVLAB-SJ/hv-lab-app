aa977c115a8dc11343d1a0b4214f0ee60b6ab2cc
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-12-01 06:51:59 +0900

    세로모드 업무요청: 요청일/요청자, 마감일/담당자 높이 정렬 (44px)

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-BAAeZP1s.css",
+    "url": "assets/index-b61y_c7C.css",
     "revision": null
   }, {
-    "url": "assets/index-q5RW68fL.js",
+    "url": "assets/index-BMXirG1A.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "a88a6efc9883a0a40583f492b24fc5cb"
+    "revision": "d43df5d733e57cec8e42ee629684e3c4"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
