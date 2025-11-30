2bdc39af7ae02fa742e17f59e2c58459cd72cf27
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-12-01 00:58:37 +0900

    업무요청: 세로모드 요청내용 입력칸 높이 축소

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-Bo8K5HZB.css",
+    "url": "assets/index-86eVJjE9.js",
     "revision": null
   }, {
-    "url": "assets/index-CKEd_-q4.js",
+    "url": "assets/index-Bkv-I3qQ.css",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "4bda6af3c68eab27b11a90f5a50856ee"
+    "revision": "60a504da3de1a423e7d5a2d469074e70"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
