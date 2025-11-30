68daf1eab83a42115959c634ca35fd00a6040032
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-30 18:11:24 +0900

    결제요청: 세로모드 상단 카드 높이 auto로 변경

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-CZPShxZW.css",
+    "url": "assets/index-CW_LeCuB.css",
     "revision": null
   }, {
-    "url": "assets/index-EsuE18Ni.js",
+    "url": "assets/index-DuV1Wv33.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "f767ae07a293764d55afabf709ebaf00"
+    "revision": "e0e71437f2303d68c5e31f2c2ba1b950"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
