21e5d810ec6b21af57b84243484d53a58aa4e309
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-11-30 14:16:02 +0900

    결제요청: 세로모드 계좌정보 2x2 그리드 배치 수정

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-BbP0uBJm.js",
+    "url": "assets/index-Bi4le9zz.css",
     "revision": null
   }, {
-    "url": "assets/index-BHNjlfjh.css",
+    "url": "assets/index-CV0OqPEK.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "39f6a4acc96efd50bfa9629b027349e9"
+    "revision": "635d8457632350fa47061b1db199789d"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
