e9f76855c0d901a11fc6005a9801815af7400f3f
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-12-01 01:00:19 +0900

    스펙북: 세로모드 카테고리 6열로 변경

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-86eVJjE9.js",
+    "url": "assets/index-hnPkKMZ-.css",
     "revision": null
   }, {
-    "url": "assets/index-Bkv-I3qQ.css",
+    "url": "assets/index-tDUDN_oz.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "60a504da3de1a423e7d5a2d469074e70"
+    "revision": "133a37e1acdfe3b6948ccf44cd19249d"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
