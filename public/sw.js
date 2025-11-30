ed425c4af45898a86eddc8530bb61e330f1dbeec
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-12-01 00:28:23 +0900

    일정표: 세로모드 캘린더 화면 전체 채우기

--- public/sw.js
+++ public/sw.js
@@ -78,10 +78,10 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index-CeQZKT5X.js",
+    "url": "assets/index-CCxP2grq.js",
     "revision": null
   }, {
-    "url": "assets/index-fwe3DEAF.css",
+    "url": "assets/index-ftngYkox.css",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "f4d266ff08c33b059edfd2906461cf2c"
+    "revision": "5a8258a928635b8c17017a3ad581cf9b"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
