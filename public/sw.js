36984f875b974ab2bd7695f2561d811f9a571d50
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-12-04 23:44:29 +0900

    실행내역: 이미지 로컬 저장으로 변경 - 모든 레코드 이미지 IndexedDB 저장

--- public/sw.js
+++ public/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "url": "assets/index-BKNIOvEE.css",
     "revision": null
   }, {
-    "url": "assets/index-DHzBFUMS.js",
+    "url": "assets/index-BWS-eUpH.js",
     "revision": null
   }, {
     "url": "assets/vendor-calendar-ZgrlWMy2.js",
@@ -115,7 +115,7 @@ define(['./workbox-f969db16'], (function (workbox) { 'use strict';
     "revision": "13c8dc084d8e721d3dbb3d62d72ae126"
   }, {
     "url": "index.html",
-    "revision": "19195a22b892e12ee28c782387b369ab"
+    "revision": "52e05e967f87565421a878a21501444a"
   }, {
     "url": "vite.svg",
     "revision": "8e3a10e157f75ada21ab742c022d5430"
